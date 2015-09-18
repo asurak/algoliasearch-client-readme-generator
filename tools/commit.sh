9252bcdcbf6daa330ecafcb95c96f538ba74d5ef
Pixelastic <tim@pixelastic.com>
2015-09-18 15:36:08 +0200

    Improve `tools.sh` and update readme

--- tools/commit.sh
+++ tools/commit.sh
@@ -1,22 +1,30 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
-cd "$(dirname "$0"/..)"
+cd "$(dirname "$0")/.."
 
+# Clean the staging index of all sub-repos, reverting to the latest commit
 if [ "$#" = "1" -a "$1" = "--revert" ] ; then
   echo "ALL README.md FILES ARE GOING TO BE RESET, HIT CTRL-C TO STOP."
   sleep 10
   for dir in ../algoliasearch-client-*; do
+    if [[ $dir =~ readme-generator ]]; then
+      continue;
+    fi
     cd "$dir"
     echo "$dir"
-    git reset ^HEAD
+    git reset --hard
   done
   exit 0
 fi
 
+# Show the diff of all subrepos
 if [ "$#" = "1" -a "$1" = "--show" ] ; then
   echo "Generate README.mds"
   ruby ./doc-generator.rb config.json
   for dir in ../algoliasearch-client-*; do
+    if [[ $dir =~ readme-generator ]]; then
+      continue;
+    fi
     cd "$dir"
     echo "$dir"
     git diff README.md
@@ -24,10 +32,14 @@ if [ "$#" = "1" -a "$1" = "--show" ] ; then
   exit 0
 fi
 
+# Update all subrepos to the latest master
 if [ "$#" = "1" -a "$1" = "--prepare" ] ; then
   echo "prepare repos to update README.mds"
   ruby ./doc-generator.rb config.json
   for dir in ../algoliasearch-client-*; do
+    if [[ $dir =~ readme-generator ]]; then
+      continue;
+    fi
     cd "$dir"
     echo "$dir"
     git checkout README.md; git pull -r
@@ -48,12 +60,19 @@ if [ "$?" != "0" ] ; then
   exit 1
 fi
 
+
+echo "Enter the commit message:"
+read commitMessage
+
 for dir in ../algoliasearch-client-*; do
+  if [[ $dir =~ readme-generator ]]; then
+    continue;
+  fi
   echo "$dir/README.md"
   sleep 1
   cd "$dir"
   git diff README.md
-  git commit README.md
+  git commit README.md -m "$commitMessage"
   git pull -r
   if [ "$?" != "0" ] ; then
     echo "Pull refused stop"
