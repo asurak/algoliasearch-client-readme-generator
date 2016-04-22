27e4dbb88a4a8b8804a299774e28151b3432c1ab
CBaptiste <coquellebaptiste@gmail.com>
2016-04-22 17:08:18 +0200

    fixing indentation

--- send-pull-request.rb
+++ send-pull-request.rb
@@ -4,15 +4,20 @@ require 'rubygems'
 require 'octokit'
 
 class SendPullRequest
+  def initialize
+    @repo = ARGV[0]
+  end
 
-    def initialize
-      @repo = ARGV[0]
-    end
-
-    def send
-        client = Octokit::Client.new(:access_token => ENV["GH_TOKEN"])
-        client.create_pull_request("algolia/"+@repo,"master","algoliareadmebot:master","Update readme", "Update read me")
-    end
+  def send
+    client = Octokit::Client.new(:access_token => ENV["GH_TOKEN"])
+    client.create_pull_request(
+      "algolia/#{@repo}",
+      "master",
+      "algoliareadmebot:master",
+      "Update readme",
+      "Update read me"
+    )
+  end
 end
 
 mail = SendPullRequest.new
